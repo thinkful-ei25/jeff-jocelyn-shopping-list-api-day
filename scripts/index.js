330b9f1143759e77bb1b1b71c1ce94ca596cdf85
jsantiag <jsantiag@wellesley.edu>
2018-10-03 15:05:05 -0500

    handleItemCheckClicked

--- scripts/index.js
+++ scripts/index.js
@@ -15,34 +15,5 @@ $(document).ready(function() {
   });
 });
 
-// api.getItems((items) =>{
-  // const item = items[0];
-
-  // api.updateItem(item.id, { name: 'fooooobar'}, () => {
-    // console.log('updated!');
-    ///////////////////////////////////
-    
-  // });
-
-
-// const item = store.items[0];
-// console.log('current name: ' + item.name);
-// store.findAndUpdate(item.id, { name: 'foobar' });
-// console.log('new name: ' + item.name);
-
-// store.items.push(Item.create("apples"));
-
-
-// api.getItems(function(data) {
-//   console.log(data);
-// });
-
-// console.log(api.BASE_URL);
-
-// api.createItem('pears', (newItem) => {
-//   api.getItems((items) => {
-//     console.log(items);
-//   });
-// });
 
 
